bbd5883a809af3e4255f668cb3cf27b6a6d4af88
Kender <kendermendoza591@gmail.com>
2024-09-21 19:45:46 +0200

    feat-test-env: refactor

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -1,5 +1,5 @@
-import { useFetchGifs } from "../hooks/useFetchGifs"
 import { CategoryData, Category } from "../types/types"
+import { useFetchGifs } from "../hooks/useFetchGifs"
 
 export const CategorySection: React.FC<Category> = ({ category }) => {
 
--- src/hooks/useFetchGifs.ts
+++ src/hooks/useFetchGifs.ts
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import { GifData } from "../types/types";
 
